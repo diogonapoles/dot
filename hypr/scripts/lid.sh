dd74d8f1516d8c2fe8e3753c119a7ed0b567e3bc
diogonapoles <napolesdr@gmail.com>
2025-11-08 13:45:24 +0000

    feat(hyprland): better monitor setup

--- hypr/scripts/lid.sh
+++ hypr/scripts/lid.sh
@@ -3,10 +3,15 @@
 if hyprctl monitors | grep -q "Monitor DP-[0-9]"; then
   if [[ "$1" == "close" ]]; then
     hyprctl keyword monitor "eDP-1,disable"
+    hyprctl keyword monitor "desc:LG Electronics LG ULTRAFINE 210MATWMYU41,3840x2160@60.00,auto,1.666667"
   elif [[ "$1" == "open" ]]; then
-    hyprctl keyword monitor "eDP-1,highres,3840x0,auto"
+    hyprctl keyword monitor "eDP-1,2880x1920@120.00,160x352,2.00"
+    hyprctl keyword monitor "desc:LG Electronics LG ULTRAFINE 210MATWMYU41,3840x2160@60.00,1600x0,1.666667"
   fi
 else
-  hyprctl keyword monitor "eDP-1,highres,3840x0,auto"
   hyprlock
+
+  if ! hyprctl monitors | grep -q "Monitor eDP-1"; then
+    hyprctl keyword monitor "eDP-1,2880x1920@120.00,auto,2.00"
+  fi
 fi
